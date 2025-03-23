34fb35a66394d86df3a3b952857fd341f5b16d9a
DUSAWANT3 <durgesh3230@gmail.com>
2025-03-23 20:44:57 +0530

    Push code 01

---
+++ src/test/java/org/dutesting/Selenium_Basic01/Ex01_Selenium_Basic.java
@@ -0,0 +1,21 @@
+package org.dutesting.Selenium_Basic01;
+
+import jdk.javadoc.internal.doclets.toolkit.util.NewAPIBuilder;
+import org.junit.Test;
+import org.junit.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class Ex01_Selenium_Basic {
+
+    @Test
+    public void test_VerifyVWOLogin() {
+        // Set path to chromedriver if not already in system PATH
+        // System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://app.vwo.com");
+        System.out.println("Page title: " + driver.getTitle());
+        driver.quit();
+    }
+}
