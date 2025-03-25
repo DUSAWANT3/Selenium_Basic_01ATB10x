7b6f15ed7f7d56488f63315e8002eeb91862203a
DUSAWANT3 <durgesh3230@gmail.com>
2025-03-25 12:26:26 +0530

    Push 25.07

--- src/test/java/org/dutesting/Selenium_Basic01/Ex01_Selenium_Basic.java
+++ src/test/java/org/dutesting/Selenium_Basic01/Ex01_Selenium_Basic.java
@@ -1,27 +1,28 @@
 package org.dutesting.Selenium_Basic01;
 
-import jdk.javadoc.internal.doclets.toolkit.util.NewAPIBuilder;
-import org.junit.Test;
 import org.junit.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Ex01_Selenium_Basic {
 
-
     @Test
     public void test_VerifyVWOLogin() {
-        // Set path to chromedriver if not already in system PATH
-        // System.setProperty("webdriver.chrome.driver", "/path/to/chromedriver");
+        // Set path to geckodriver if not already in system PATH
+        // System.setProperty("webdriver.gecko.driver", "/path/to/geckodriver");
 
-       /* WebDriver driver = new ChromeDriver();
-        driver.get("https://app.vwo.com");
-        System.out.println("Page title: " + driver.getTitle());
-        driver.quit(); */
+        // Write the code which will execute and perform the UI interactions
+        //FirefoxDriver driver = new FirefoxDriver();
 
-        FirefoxDriver driver = new FirefoxDriver();
+       // WebDriver driver = new ChromeDriver();
+
+        WebDriver driver = new EdgeDriver();
         driver.get("https://app.vwo.com");
         System.out.println(driver.getTitle());
+
+        // Optional: Close the browser after the test
+        driver.quit();
     }
 }
