3a170d1f607fdcc9f9db33b8517f2156a5cb7242
DUSAWANT3 <durgesh3230@gmail.com>
2025-03-31 12:13:39 +0530

    Push 31.02

---
+++ src/test/java/org/dutesting/Selenium_Basic01/Ex04_LintText.java
@@ -0,0 +1,37 @@
+package org.dutesting.Selenium_Basic01;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Ex04_LintText {
+
+
+    @Test
+    @Description("click on llink Text")
+
+    public void clickonLinktext() throws InterruptedException {
+
+        WebDriver driver = new EdgeDriver();
+        driver.get("https://app.vwo.com/#/login");
+        driver.manage().window().maximize();
+
+      // <a href="https://vwo.com/free-trial/?utm_medium=website&utm_source=login-page&utm_campaign=mof_eg_loginpage"
+        // class="text-link"
+        // data-qa="bericafeqo">Start a free trial</a>
+
+        WebElement LinkText = driver.findElement(By.linkText("Start a free trial"));
+        LinkText.click();
+
+        Thread.sleep(3000);
+        Assert.assertEquals(driver.getCurrentUrl(),"https://vwo.com/free-trial/?utm_medium=website&utm_source=login-page&utm_campaign=mof_eg_loginpage");
+
+        driver.close();
+    }
+
+
+}
