ddd7921890450a9fb0290a672f815b9535c2e81d
DUSAWANT3 <durgesh3230@gmail.com>
2025-04-02 15:38:36 +0530

    Push OPenCart Demo 02.02

--- src/test/java/org/dutesting/Selenium_BasicProject03/Pro04_OpenCartDemo.java
+++ src/test/java/org/dutesting/Selenium_BasicProject03/Pro04_OpenCartDemo.java
@@ -2,8 +2,11 @@ package org.dutesting.Selenium_BasicProject03;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Owner;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class Pro04_OpenCartDemo {
@@ -13,14 +16,32 @@ public class Pro04_OpenCartDemo {
     @Test
     @Description("Verify Open Cart Demo web Application")
     @Owner("Durgesh Sawant")
-    public void OpenCartDemoVerify(){
-
+    public void OpenCartDemoVerify() {
         WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
 
         driver.get("https://www.opencart.com/index.php?route=common/home");
 
-        //click on Register buttton
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getCurrentUrl(), "https://www.opencart.com/index.php?route=common/home");
+
+        //click on Register button
+        WebElement RegisterButton = driver.findElement(By.xpath("//a[@class=\"btn btn-black navbar-btn\"]"));
+        RegisterButton.click();
+
+        Assert.assertEquals(driver.getCurrentUrl(),"https://www.opencart.com/index.php?route=account/register");
+
+        WebElement UsernameField = driver.findElement(By.xpath("//*[@id=\"input-username\"]"));
+        UsernameField.sendKeys("Raj123");
+
+        WebElement FirstnameField = driver.findElement(By.xpath("//*[@id=\"input-firstname\"]"));
+        UsernameField.sendKeys("Raj");
+
+        WebElement LastnameField = driver.findElement(By.xpath("//*[@id=\"input-lastname\"]"));
+        UsernameField.sendKeys("OK");
+
+        WebElement EmailField = driver.findElement(By.xpath("//*[@id=\"input-email\"]"));
+        UsernameField.sendKeys("OKRAJ@email.com");
 
 
     }
