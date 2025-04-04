7a9c3f605f79e77f41ab15dbf1957c210ac8424f
DUSAWANT3 <durgesh3230@gmail.com>
2025-04-04 16:46:01 +0530

    Push OPenCart Demo 04.02

---
+++ src/test/java/org/dutesting/Selenium_Basic02/Ex04_HandelsDropdowns.java
@@ -0,0 +1,49 @@
+package org.dutesting.Selenium_Basic02;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.support.ui.Select;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Ex04_HandelsDropdowns {
+
+    @Test
+    @Description("Verify and select Dropdoen Box")
+    public void HaandleDropdon(){
+
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://www.testautomationcentral.com/demo/dropdown.html");
+    //Click on simple dropdown box but-
+        driver.findElement(By.xpath("//button[@data-target=\"simple-dropdown\"]")).click();
+
+    //Static Dropdown - This is a traditional dropdown with <select> and <option> tags.
+        //Handle using Select Class //div[@id="simple-dropdown"]//select
+
+        WebElement SimpleDropdown = driver.findElement(By.xpath("//div[@id=\"simple-dropdown\"]//select"));
+        Select select = new Select(SimpleDropdown);
+
+        //Select by visible text
+        select.selectByVisibleText("Option 3");
+
+        // Get selected option
+        String selectedValue = select.getFirstSelectedOption().getText();
+        // Verify the selected value
+        Assert.assertEquals(selectedValue, "Option 3", "Selected value is incorrect!");
+
+       //Select by Value Attribute
+        select.selectByValue("option2");
+
+        //Select by Index
+        select.selectByIndex(0);
+        String selectedValue1 = select.getFirstSelectedOption().getText();
+        Assert.assertEquals(selectedValue1, "Option 1", "Selected value is incorrect!");
+
+        driver.close();
+    }
+}
