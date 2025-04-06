e77539e68386ca6cdcacd9b3cb98e65135bee78d
DUSAWANT3 <durgesh3230@gmail.com>
2025-04-06 22:52:42 +0530

    Push OPenCart Demo 06.05

--- src/test/java/org/dutesting/Selenium_Basic05_SVG_Elements/Ex01_SVG_Elements.java
+++ src/test/java/org/dutesting/Selenium_Basic05_SVG_Elements/Ex01_SVG_Elements.java
@@ -13,38 +13,50 @@ import org.testng.annotations.Test;
 
 import java.util.List;
 
+//1.	SVG - - Scalable Vector Graphics to define graphics for the web.
+//2.	XML-based language to create 2-D graphics/images with animation and interactivity.
+//3.	Use geometrical figures to draw an image.
+
 public class Ex01_SVG_Elements {
 
-     WebDriver driver;
-    @BeforeTest
-    public void openBrowser(){
-        WebDriver driver = new EdgeDriver();
-        driver.manage().window().maximize();
+    WebDriver driver;
 
+    @BeforeTest
+    public void openBrowser() {
+        driver = new EdgeDriver();  // fixed redeclaration issue
+        driver.manage().window().maximize();
+        driver.get("https://www.flipkart.com/search");
     }
 
-    @Test
-    @Description("Verify the search with SVG icon resultes are visible.")
-    public void test_Flipkart_Search_SVG_Icon(){
-        driver.get("https://www.google.co.in/");
-        
-      //Step 1 - Enter the "mac" in inputBox.
-       WebElement searchbox = driver.findElement(By.xpath("//*[@name=\"q\"]"));
-        searchbox.sendKeys("MAC");
+    @Test(description = "Verify the search with SVG icon results are visible.")
+    public void test_Flipkart_Search_SVG_Icon() throws InterruptedException {
+        // Step 1 - Enter the "MACMINI" in inputBox
+        WebElement searchbox = driver.findElement(By.name("q"));
+        searchbox.sendKeys("MACMINI");
 
-        //SVG x path -//*[name()='svg']
-        //Step 2 find all the SVG elements on page
+        // Step 2 - Click the SVG search icon
         List<WebElement> svgElements = driver.findElements(By.xpath("//*[name()='svg']"));
-        svgElements.get(4).click();
+        if (!svgElements.isEmpty()) {
+            svgElements.get(0).click();
+        } else {
+            System.out.println("SVG element not found.");
+            return;
+        }
 
+        // Wait (not ideal, but since you haven't used WebDriverWait yet)
+        Thread.sleep(3000);
 
-
+        // Step 3 - Print search result titles
+        List<WebElement> titlesResults = driver.findElements(By.xpath("//div[contains(@data-id,'CPU')]/div/a[2]"));
+        for (WebElement title : titlesResults) {
+            System.out.println(title.getText());
+        }
     }
 
     @AfterTest
-    public void CloseBrowser() throws InterruptedException {
-        Thread.sleep(3000);
-  driver.close();
+    public void closeBrowser() {
+        if (driver != null) {
+            driver.quit();
+        }
     }
-
 }
