6456a12aaea7ae664837109b3e3c03792dc9a5c3
DUSAWANT3 <durgesh3230@gmail.com>
2025-04-07 11:55:27 +0530

    Push OPenCart Demo 07.02

---
+++ src/test/java/org/dutesting/Selenium_BasicProject03/Pro06_katalon_demoE2E.java
@@ -0,0 +1,52 @@
+package org.dutesting.Selenium_BasicProject03;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class Pro06_katalon_demoE2E {
+    //Open the URL - https://katalon-demo-cura.herokuapp.com/
+    //Enter the username and password
+    //Verify that the current URL changes to https://katalon-demo-cura.herokuapp.com/#appointment
+    @Test
+    @Description("Verify that the current URL changes to")
+
+    public void testVerifyKatalonDemo(){
+
+        WebDriver driver = new ChromeDriver();
+        driver.get("https://katalon-demo-cura.herokuapp.com/");
+        driver.manage().window().maximize();
+        // driver.manage().deleteAllCookies(); //To Delete all the cookies
+
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getCurrentUrl(), "https://katalon-demo-cura.herokuapp.com/");
+
+        //Click on make appointment
+        WebElement MakeAppointment = driver.findElement(By.id("btn-make-appointment"));
+        MakeAppointment.click();
+
+        //Enter username and Password
+        WebElement UserName = driver.findElement(By.id("txt-username"));
+        UserName.sendKeys("John Doe");
+
+        WebElement Password = driver.findElement(By.id("txt-password"));
+        Password.sendKeys("ThisIsNotAPassword");
+
+        //click on Login button
+        WebElement LoginBut = driver.findElement(By.id("btn-login"));
+        LoginBut.click();
+
+        //Verify that the current URL changes to https://katalon-demo-cura.herokuapp.com/#appointment
+        System.out.println(driver.getCurrentUrl());
+        Assert.assertEquals(driver.getCurrentUrl(), "https://katalon-demo-cura.herokuapp.com/#appointment");
+
+        //on Katalon -Make Appointment page fill all information
+
+
+       // driver.quit();
+    }
+}
