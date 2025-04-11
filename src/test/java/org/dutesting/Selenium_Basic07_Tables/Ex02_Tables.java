a433a59e34dddea3303c6340093f81c31c027991
DUSAWANT3 <durgesh3230@gmail.com>
2025-04-11 10:18:01 +0530

    Push OPenCart Demo 11.03

---
+++ src/test/java/org/dutesting/Selenium_Basic07_Tables/Ex02_Tables.java
@@ -0,0 +1,38 @@
+package org.dutesting.Selenium_Basic07_Tables;
+
+import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.annotations.Test;
+
+import java.util.List;
+
+public class Ex02_Tables {
+
+    @Test
+    @Description("Verify and validate tables")
+    public void handelsTabels() {
+
+        WebDriver driver = new EdgeDriver();
+        driver.manage().window().maximize();
+
+        driver.get("https://awesomeqa.com/webtable1.html");
+
+        WebElement table = driver.findElement(By.xpath("//table[@summary=\"Sample Table\"]/tbody"));
+
+        //roe and columns
+        List<WebElement> row_table = table.findElements(By.tagName("tr"));
+
+        for (int i = 0; i < row_table.size(); i++) {
+            List<WebElement> col = row_table.get(i).findElements(By.tagName("td"));
+            for (WebElement c : col) {
+                System.out.print(c.getText() + " | ");
+            }
+            System.out.println();
+        }
+
+        driver.close();
+    }
+}
