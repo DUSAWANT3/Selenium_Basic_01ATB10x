5da09f71d64214ad83ecd92ca024c03b57a0a81d
DUSAWANT3 <durgesh3230@gmail.com>
2025-04-18 11:57:02 +0530

    18.02

--- src/test/java/org/dutesting/Selenium_Basic08_ActionClass/Ex01_ActionClass.java
+++ src/test/java/org/dutesting/Selenium_Basic08_ActionClass/Ex01_ActionClass.java
@@ -1,9 +1,15 @@
 package org.dutesting.Selenium_Basic08_ActionClass;
 
 import io.qameta.allure.Description;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.interactions.Actions;
 import org.testng.annotations.Test;
+
+import javax.swing.*;
 
 public class Ex01_ActionClass {
 
@@ -12,8 +18,20 @@ public class Ex01_ActionClass {
     //GreenCart is E-commerce website
     public void ActionClass() throws InterruptedException {
         WebDriver driver = new EdgeDriver();
-        driver.get("https://testautomationpractice.blogspot.com/"); //URL
+        driver.get("https://demoqa.com/text-box"); //URL
         driver.manage().window().maximize();
+
+        Thread.sleep(2000);
+        //Key -Action Classes
+        //Press Shift button and send Key  - so it type in Capitial
+        WebElement firstName = driver.findElement(By.id("userName"));
+
+        Actions actions = new Actions(driver);
+       actions
+               .keyDown(Keys.SHIFT)
+               .sendKeys(firstName,"durgesh")
+               .keyUp(Keys.SHIFT).build().perform();
+
 
     }
 }
